b03f0c380a11e51afa5de8d6ac2233a8e8a5a508
mh <mh@immerda.ch>
2008-05-29 17:02:41 +0000

    merged from puzzle

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,18 @@
-#######################################
+#
 # acpid module
-# Puzzle ITC - haerry+puppet(at)puzzle.ch
-# GPLv3
-#######################################
+#
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # modules_dir { "acpid": }
+
 class acpid {
     include acpid::base 
 }
